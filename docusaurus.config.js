883b9a20334c85d92f46e7532fddc2053858a4f2
rachels-archive <rachelhui@protonmail.com>
2023-09-14 16:13:58 +0800

    tagline changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Notes by Rachel',
-  tagline: 'CS50x2023 Notes, Solutions and Project showcase',
+  tagline: 'CS50x 2023 Notes, Solutions and Project showcase',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
